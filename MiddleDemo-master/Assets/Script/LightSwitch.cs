18ab33d9c88ee0c16dfca208c73cfda1ff669d13
emma <emma.jieun.oh@gmail.com>
2019-04-20 19:22:55 +0900

    commit

---
+++ MiddleDemo-master/Assets/Script/LightSwitch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightSwitch : MonoBehaviour
+{
+    private bool onTrigger;
+    private bool active;
+
+    [SerializeField]
+    private GameObject l_light;
+
+    [SerializeField]
+    private Text l_text;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        onTrigger = true;
+        if (!l_light.activeSelf)
+        {
+            l_text.text = "불을 키려면" + "<color=yellow>" + "(F)" + "</color>";
+        }
+        else {
+            l_text.text = "불을 끄려면" + "<color=yellow>" + "(F)" + "</color>";
+        }
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        onTrigger = false;
+        l_text.text = "";
+    }
+
+    void Start()
+    {
+        l_light.SetActive(false);
+        l_text.text = "";
+    }
+    void Update()
+    {
+        if (onTrigger)
+        {
+
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                active = !active;
+                l_text.text = "";
+            }
+        }
+
+        if (active)
+        {
+            l_light.SetActive(true);
+        }
+        else
+        {
+            l_light.SetActive(false);
+        }
+    }
+}
---
+++ MiddleDemo-master/Assets/Script/flashLight.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class flashLight : MonoBehaviour
+{
+    private bool onTrigger;
+    
+    [SerializeField]
+    private Text l_text;
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        onTrigger = true;
+        l_text.text = "획득하려면" + "<color=yellow>" + "(F)" + "</color>";
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        onTrigger = false;
+        l_text.text = "";
+    }
+
+    void Start()
+    {
+        (GameObject.FindGameObjectWithTag("flash")).SetActive(false);
+        (GameObject.FindGameObjectWithTag("flashLight")).SetActive(false);
+        (GameObject.FindGameObjectWithTag("flashTrigger")).SetActive(true);
+    }
+    void Update()
+    {
+        if (onTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                l_text.text = "";
+                (GameObject.FindGameObjectWithTag("flash")).SetActive(true);
+                (GameObject.FindGameObjectWithTag("flashLight")).SetActive(true);
+                (GameObject.FindGameObjectWithTag("flashTrigger")).SetActive(false);
+            }
+        }
+    }
+}
